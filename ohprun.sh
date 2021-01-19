d03b5dc2600ae7307a8899980efeaecf3622cc7b
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2021-01-19 18:46:19 +0000

    Delete ohprun.sh

--- ohprun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-screen -dm bash -c "./ohpserver -port 5555 -proxy 127.0.0.1:1130 -tunnel 127.0.0.1:225"
-screen -dm bash -c "./ohpserver -port 7788 -proxy 127.0.0.1:1130 -tunnel 127.0.0.1:990"
