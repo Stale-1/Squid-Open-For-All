2714d2a149503a6a4110b30e779a037ee9d71a32
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2021-05-08 01:24:16 +0800

    Update nossh.sh

--- nossh.sh
+++ nossh.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Install Adguard
-apt update
-apt-get install sudo nano bind9-host -y
-wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz
-tar xvf AdGuardHome_linux_amd64.tar.gz
-cd /root/AdGuardHome && pwd
-cd /root/AdGuardHome && ./AdGuardHome -s install
-
 wget https://raw.githubusercontent.com/Stale-1/Squid-Open-For-All/main/sources.list -O /etc/apt/sources.list
 #
 apt-get update
