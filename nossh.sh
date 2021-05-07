8835a328cc9282ce577cadd34890a9737f0565c6
Stale-1 <74855954+Stale-1@users.noreply.github.com>
2021-05-08 01:09:36 +0800

    Update nossh.sh

--- nossh.sh
+++ nossh.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/Stale-1/Squid-Open-For-All/main/sources.list -O /etc/apt/sources.list
-
+#
 apt-get update
 apt- get upgrade -y
 apt-get install squid -y
-sed -i "s/http_port 3128/http_port 1130/g" /etc/squid3/squid.conf
-sed -i "s/http_access deny all/http_access allow all/g" /etc/squid3/squid.conf
+wget https://raw.githubusercontent.com/Stale-1/Squid-Open-For-All/main/squid.conf -O /etc/squid3/squid.conf
+# Install Adguard
+apt-get install sudo nano bind9-host
+wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz
+tar xvf AdGuardHome_linux_amd64.tar.gz
+cd /root/AdGuardHome && pwd
+cd /root/AdGuardHome && ./AdGuardHome -s install
+
+
